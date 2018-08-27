6e7f454ca7d3f0675e91c1b8c1268ff648b8b27c
Karl-Martin Uiga <kauiga@ttu.ee>
2018-08-27 22:32:57 +0300

    Flexbox for social buttons

--- src/containers/SocialBar/SocialBar.js
+++ src/containers/SocialBar/SocialBar.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import ReactTransitionGroup from 'react-addons-css-transition-group'; // ES6
 import { SocialIcon } from 'react-social-icons';
+import './SocialBar.css';
+
 
 class SocialBar extends Component {
 
 	render() {
 		return (
-			<div>
-				<SocialIcon url="http://linkedin.com/in/jaketrent" />
+			<div className="container">
+				<SocialIcon url="https://www.linkedin.com/in/karl-martin-uiga-aa7131114/" color="#532D00" />
+				<SocialIcon url="https://www.facebook.com/karlmartin.uiga?ref=bookmarks" color="#532D00" />
+				<SocialIcon url="https://github.com/martinuiga" color="#532D00" />
+				<SocialIcon url="https://www.linkedin.com/in/karl-martin-uiga-aa7131114/" color="#532D00" />
+				<SocialIcon url="https://www.facebook.com/karlmartin.uiga?ref=bookmarks" color="#532D00" />
+				<SocialIcon url="https://github.com/martinuiga" color="#532D00" />
+				<SocialIcon url="https://www.linkedin.com/in/karl-martin-uiga-aa7131114/" color="#532D00" />
+				<SocialIcon url="https://www.facebook.com/karlmartin.uiga?ref=bookmarks" color="#532D00" />
+				<SocialIcon url="https://github.com/martinuiga" color="#532D00" />
+				<SocialIcon url="https://www.linkedin.com/in/karl-martin-uiga-aa7131114/" color="#532D00" />
+				<SocialIcon url="https://www.facebook.com/karlmartin.uiga?ref=bookmarks" color="#532D00" />
+				<SocialIcon url="https://github.com/martinuiga" color="#532D00" />
+				<SocialIcon url="https://www.linkedin.com/in/karl-martin-uiga-aa7131114/" color="#532D00" />
+				<SocialIcon url="https://www.facebook.com/karlmartin.uiga?ref=bookmarks" color="#532D00" />
+				<SocialIcon url="https://github.com/martinuiga" color="#532D00" />
 			</div>
 		)
 	}
